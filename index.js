7ff839c221ec7440fc45c6042cb36d261acae32b
VenneriGiacomo <vennerigiacomo98@gmail.com>
2023-03-28 23:32:21 +0200

    index

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const path = require("path");
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 
 app.get('/', (req, res) => {
-  
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname, "/views/index.html"));
 });
 
 app.listen(port, () => {
